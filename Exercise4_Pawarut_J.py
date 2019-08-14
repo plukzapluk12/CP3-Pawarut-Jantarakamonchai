4bed2e3afee3f6cfef2fb6bbfb6e9a41cde3f00a
plukzapluk12 <pluk1231@gmail.com>
2019-08-14 22:37:44 +0700

    Create Exercise4_Pawarut_J.py

---
+++ Exercise4_Pawarut_J.py
@@ -0,0 +1,9 @@
+f = 99.99
+c = 9.99
+i = 10.00
+g = 11.11
+print("--- Your Score ---")
+print("Foundation English",":",f)
+print("General Business",":",c)
+print("Introduction to Computer Systems",":",i)
+print("Computer Programming",":",g)
